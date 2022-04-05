eb9869e6b861e958d97915c16167c4479a4adb7f
Bruce762 <82109931+Bruce762@users.noreply.github.com>
2022-04-05 15:37:04 +0800

    Add files via upload

--- bodydetect2.py
+++ bodydetect2.py
@@ -1,14 +1,14 @@
-from cgi import print_arguments
-from pickle import TRUE
 import cv2
 from cv2 import FILLED
 import mediapipe as mp
 import numpy as np
 import mss
 import keyboard
-import win32api
-import win32con
 import time
+import ctypes, sys
+import mouse
+
+
 
 mpPose = mp.solutions.pose
 poses = mpPose.Pose()
@@ -19,34 +19,52 @@ poseconStyle = mpDraw.DrawingSpec(color=(0, 255, 0), thickness=10)
 sct = mss.mss()#mss截圖
 top = 360
 left = 480
-width = 350
-height = 350
+width = 200
+height = 200
 monitor = {"top": top, "left": left, "width": width, "height": height}#截圖方框位置
+istart = True
 
-while True:
-    t=time.time()
-    image = np.array(sct.grab(monitor))#截圖
-    imgHeigh = image.shape[0]
-    imgWide = image.shape[1]
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    results = poses.process(image)
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.pose_landmarks:  
-        mpDraw.draw_landmarks(image, results.pose_landmarks, mpPose.POSE_CONNECTIONS, poseLmsStyle, poseconStyle)
-        for i, lm in enumerate(results.pose_landmarks.landmark):
-            if i == 0 and lm:
-                xPos = lm.x* imgWide
-                yPos = lm.y* imgHeigh
-                xPos = int(xPos)
-                yPos = int(yPos)
-                image = cv2.circle(image, (xPos,yPos), 4, (255, 0, 0), FILLED)
-                if keyboard.is_pressed("v"):
-                    x=int((lm.x-0.5)*imgWide)
-                    y=int((lm.y-0.5)*imgHeigh)
-                    win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, x, y, 0, 0)
-                break
-    image = cv2.resize(image,(0,0),fx=1.5,fy=1.5)
-    cv2.imshow('MediaPipe Pose', image)
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
+def is_admin():
+    #檢測當前程式是不是管理員模式
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+if is_admin():
+    #Code of your program here        
+    while True:
+        if(keyboard.is_pressed('o')):
+            istart = True
+            print("mode : open")
+        if(keyboard.is_pressed('p')):
+            istart = False
+            print("mode : close")
+        t=time.time()
+        image = np.array(sct.grab(monitor))#截圖
+        imgHeigh = image.shape[0]
+        imgWide = image.shape[1]
+        if(mouse.is_pressed() & istart):
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            results = poses.process(image)
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            if results.pose_landmarks:  
+                mpDraw.draw_landmarks(image, results.pose_landmarks, mpPose.POSE_CONNECTIONS, poseLmsStyle, poseconStyle)
+                for i, lm in enumerate(results.pose_landmarks.landmark):
+                    if i == 0 and lm:
+                        xPos = lm.x* imgWide
+                        yPos = lm.y* imgHeigh
+                        xPos = int(xPos)
+                        yPos = int(yPos)
+                        image = cv2.circle(image, (xPos,yPos), 4, (255, 0, 0), FILLED)
+                        x=int((lm.x-0.5)*imgWide+20)
+                        y=int((lm.y-0.5)*imgHeigh+20)
+                        mouse.move(x, y, absolute=False, duration=0)
+                        break
+        image = cv2.resize(image,(0,0),fx=1.5,fy=1.5)
+        cv2.putText(image,"Fps :"+str(int(1/(time.time()-t))),(1,30),cv2.FONT_HERSHEY_SIMPLEX,1,(204,255,0),2)
+        cv2.imshow('MediaPipe Pose', image)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+else:
+    # Re-run the program with admin rights
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
